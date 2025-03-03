9a13dde4a3e21e159323da3670428c23552a47bf
Hudson Farias <hudson.farias.dev@gmail.com>
2025-03-03 15:51:35 -0300

    fix: prod images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/admin' : '',
+  images: {
+    loader: 'default',
+    path: process.env.NODE_ENV === 'production' ? '/admin/_next/image' : '/_next/image'
+  },
 };
 
 export default nextConfig;
