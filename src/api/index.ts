2d4e71f0e1427411cfdb5934dbce6593536107d2
Hudson Farias <hudson.farias.dev@gmail.com>
2025-03-03 18:39:01 -0300

    feat: projects table

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+class ApiClient {
+  private baseURL: string | undefined
+
+  constructor() {
+    this.baseURL = `${process.env.NEXT_PUBLIC_API_URL}/admin`
+  }
+
+  private async request(method: string, endpoint: string, body?: any): Promise<any> {
+    const response = await fetch(`${this.baseURL}${endpoint}`, {
+      method, body: body ? JSON.stringify(body) : undefined
+    })
+
+    return response
+  }
+
+  public get(endpoint: string): Promise<Response> {
+    return this.request('GET', endpoint)
+  }
+}
+
+export const API = new ApiClient()
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" data-theme="black">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+        <div className="bg-base-100 h-screen">
+
+          <div className="navbar shadow-sm">
+            <div className="flex-1">
+              <a href='/' className="btn btn-ghost text-xl">HudsonDev</a>
+            </div>
+            
+            {/* <div className="flex-none">
+              <ul className="menu menu-horizontal px-1">
+                <li><a>Link</a></li>
+                <li>
+                  <details>
+                    <summary>Parent</summary>
+                    <ul className="bg-base-100 rounded-t-none p-2">
+                      <li><a>Link 1</a></li>
+                      <li><a>Link 2</a></li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </div> */}
+          </div>
+
+
+
+          <div className="flex justify-center">
+            {children}
+          </div>
+
+        </div>
+
       </body>
     </html>
   );
